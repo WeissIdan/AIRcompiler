c2afac023b96177c38c0c1d9a485c21910b85571
unknown <i.weiss.2006@gmail.com>
2026-05-12 20:14:05 +0300

    added symtable and scopes

---
+++ symtable.c
@@ -0,0 +1,78 @@
+#include "symtable.h"
+
+Scope* current_scope = NULL;
+
+void push_scope() {
+    Scope* new_scope = (Scope*)malloc(sizeof(Scope));
+    new_scope->head = NULL;
+ 
+    new_scope->next = current_scope; 
+    
+    current_scope = new_scope; 
+}
+
+void pop_scope() {
+    if (current_scope == NULL) return;
+
+    Scope* temp_scope = current_scope;
+    current_scope = current_scope->next; 
+
+    Symbol* curr_sym = temp_scope->head;
+    while (curr_sym != NULL) {
+        Symbol* temp_sym = curr_sym;
+        curr_sym = curr_sym->next;
+        free(temp_sym->name);
+        if (temp_sym->type) free(temp_sym->type);
+        if (temp_sym->kind) free(temp_sym->kind);
+        free(temp_sym);
+    }
+    free(temp_scope);
+}
+
+int insert_symbol(char* name, char* type, char* kind) {
+    if (current_scope == NULL) push_scope(); 
+
+    if (lookup_current_scope(name) != NULL) {
+        return 0;
+    }
+
+    Symbol* new_sym = (Symbol*)malloc(sizeof(Symbol));
+    new_sym->name = strdup(name);
+    new_sym->type = type ? strdup(type) : NULL;
+    new_sym->kind = kind ? strdup(kind) : NULL;
+
+    new_sym->next = current_scope->head;
+    current_scope->head = new_sym;
+
+    return 1; 
+}
+
+Symbol* lookup_symbol(char* name) {
+    Scope* scope_runner = current_scope;
+
+    while (scope_runner != NULL) {
+        Symbol* sym_runner = scope_runner->head;
+        while (sym_runner != NULL) {
+            if (strcmp(sym_runner->name, name) == 0) {
+                return sym_runner; 
+            }
+            sym_runner = sym_runner->next;
+        }
+        scope_runner = scope_runner->next;
+    }
+    return NULL; 
+}
+
+Symbol* lookup_current_scope(char* name) {
+    if (current_scope == NULL) return NULL;
+
+    Symbol* sym_runner = current_scope->head;
+    while (sym_runner != NULL) {
+        if (strcmp(sym_runner->name, name) == 0) {
+            return sym_runner;
+        }
+        sym_runner = sym_runner->next;
+    }
+    return NULL;
+}
+
---
+++ symtable.h
@@ -0,0 +1,29 @@
+#ifndef SYMTAB_H
+#define SYMTAB_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Symbol {
+    char* name;         
+    char* type;         
+    char* kind;         
+    
+    struct Symbol* next; 
+} Symbol;
+
+typedef struct Scope {
+    Symbol* head;        
+    struct Scope* next;  
+} Scope;
+
+extern Scope* current_scope;
+
+void push_scope();                                   
+void pop_scope();                                    
+int insert_symbol(char* name, char* type, char* kind); 
+Symbol* lookup_symbol(char* name);                   
+Symbol* lookup_current_scope(char* name);            
+
+#endif
